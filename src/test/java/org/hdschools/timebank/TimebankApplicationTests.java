eff8a9a526d051a08f2da7a5f0f61dbaf6dcf398
Alex <ryanlee486@icloud.com>
2025-10-31 21:36:43 +0800

    initial commit

---
+++ src/test/java/org/hdschools/timebank/TimebankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hdschools.timebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
