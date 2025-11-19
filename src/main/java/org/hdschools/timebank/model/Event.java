762ac1851e290d6559c3ab208e493a86c6120fbe
Alex <ryanlee486@icloud.com>
2025-11-20 00:39:45 +0800

    add database entity Event

---
+++ src/main/java/org/hdschools/timebank/model/Event.java
@@ -0,0 +1,54 @@
+package org.hdschools.timebank.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "event")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "init_stu_id")
+    private String initStuId;
+
+    @Column(name = "init_sta_id")
+    private String initStaId;
+
+    @Column(name = "recv_stu_id")
+    private String recvStuId;
+
+    @Column(name = "recv_sta_id")
+    private String recvStaId;
+
+    @Column(name = "point_diff", nullable = false)
+    private int pointDiff;
+
+    @Column(name = "credit_diff", nullable = false)
+    private short creditDiff;
+
+    @Column(name = "note")
+    private String note;
+}
---
+++ src/main/java/org/hdschools/timebank/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package org.hdschools.timebank.repository;
+
+import org.hdschools.timebank.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
